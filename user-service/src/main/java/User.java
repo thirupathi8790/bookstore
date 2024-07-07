79a89f0fc4589723bf531e2ec52ea48087477954
thirupathi8790 <128212210+thirupathi8790@users.noreply.github.com>
2024-07-07 13:48:02 +0530

    Create User.java

---
+++ user-service/src/main/java/User.java
@@ -0,0 +1,13 @@
+public class User {
+  private int id;
+  private String name;
+  private String email;
+
+  public User(int id, String name, String email) {
+    (link unavailable) = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  // getters and setters
+}
